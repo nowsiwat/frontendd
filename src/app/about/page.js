5645638ae9f07cd611f735118e68785f5a2ea57f
nowsiwat <nowsiwat@gmail.com>
2024-07-08 15:20:31 +0700

    first commit

---
+++ src/app/about/page.js
@@ -0,0 +1,60 @@
+
+export default function Home() {
+  return (
+<>
+<nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">LOGO</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon" />
+    </button>
+   <div className="d-flex" id="navbarSupportedContent">
+  <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+    <li className="nav-item">
+      <a className="nav-link active" aria-current="page" href="/">Home</a>
+    </li>
+    <li className="nav-item">
+    <a className="nav-link active" aria-current="page" href="/about">About</a>
+    </li>
+    <li className="nav-item">
+    <a className="nav-link active" aria-current="page" href="/service">Service</a>
+    </li>
+    <li className="nav-item">
+    <a className="nav-link active" aria-current="page" href="/contact">Contact</a>
+    </li>
+  </ul>
+<form className="d-flex" role="navbarSupportedContent">
+<a className="btn btn-outline-secondary" type="page" href="/login" >SignIn</a>
+<a className="btn btn-outline-secondary" type="page" href="/sign" >SignUp</a>
+</form>
+
+</div>
+
+  </div>
+  </nav>
+
+  <section class="py-5">
+	<div class="container">
+		<div class="text-center">
+			<span class="text-muted">Our Story</span>
+			<h2 class="display-5 fw-bold mb-3">About Me</h2>
+		</div>
+		<div class="row gx-5">
+			<div class="col-md-6">
+				<div class="lead text-center text-md-start">
+					<p>Lorem ipsum dolor, sit amet elit Quamnitm veniam dicta architecto, quos id nemo minima nulla ducimus officii nulla ducimus officiis! quos id nemo minima nulla ducimus officii nulla ducimus officiis.</p>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="lead text-center text-md-start">
+					<p>Lorem ipsum dolor, sit amet elit Quamnitm veniam dicta architecto, quos id nemo minima nulla ducimus officii nulla ducimus officiis Quamnitm veniam dicta architecto, quos id nemo minima.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+</>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -6,28 +6,29 @@ export default function Home() {
 <>
 <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">LOGO</a>
+    <a className="navbar-brand" href="/"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/800px-Bootstrap_logo.svg.png" alt="Bootstrap" width={30} height={24} />
+</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon" />
     </button>
    <div className="d-flex" id="navbarSupportedContent">
   <ul className="navbar-nav me-auto mb-2 mb-lg-0">
     <li className="nav-item">
-      <a className="nav-link active" aria-current="page" href="http://localhost:3001/">Home</a>
+      <a className="nav-link active" aria-current="page" href="/">Home</a>
     </li>
     <li className="nav-item">
-    <a className="nav-link active" aria-current="page" href="https://www.facebook.com/siwat.sangkhaanan.5">About</a>
+    <a className="nav-link active" aria-current="page" href="/about">About</a>
     </li>
     <li className="nav-item">
-    <a className="nav-link active" aria-current="page" href="#">Service</a>
+    <a className="nav-link active" aria-current="page" href="/service">Service</a>
     </li>
     <li className="nav-item">
-    <a className="nav-link active" aria-current="page" href="#">Contact</a>
+    <a className="nav-link active" aria-current="page" href="/contact">Contact</a>
     </li>
   </ul>
 <form className="d-flex" role="navbarSupportedContent">
-<button className="btn btn-outline-secondary" type="submit">SignIn</button>
-  <button className="btn btn-outline-secondary" type="submit">SignUp</button>
+<a className="btn btn-outline-secondary" type="page" href="/login" >SignIn</a>
+<a className="btn btn-outline-secondary" type="page" href="/sign" >SignUp</a>
 </form>
 
 </div>
@@ -36,44 +37,57 @@ export default function Home() {
   </nav>
 
   <div className="container-fluid">
-      <div className="row my-2">
-      <div id="carouselExampleFade" className="carousel slide carousel-fade">
-  <div className="carousel-inner">
-    <div className="carousel-item active">
-      <img src="https://ninenic.com/download/ninenic_com/webdesign/website-builder-websitedesign-banner1.jpg" className="d-block w-100" width="600px" height="500px" />
+    <div classname="row my-2">
+  <div id="carouselExample" className="carousel slide">
+    <div className="carousel-inner">
+      <div className="carousel-item active">
+        <img src="https://images.pexels.com/photos/1402787/pexels-photo-1402787.jpeg?auto=compress&cs=tinysrgb&w=600" className="d-block w-100" width="100%" height="500px" />
+      </div>
+      <div className="carousel-item">
+        <img src="https://images.pexels.com/photos/1624255/pexels-photo-1624255.jpeg?auto=compress&cs=tinysrgb&w=600" className="d-block w-100"  width="100%"  height="500px" />
+      </div>
+      <div className="carousel-item">
+        <img src="https://images.pexels.com/photos/592077/pexels-photo-592077.jpeg?auto=compress&cs=tinysrgb&w=600" className="d-block w-100"  width="100%"  height="500px" />
+      </div>
     </div>
+    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+      <span className="carousel-control-prev-icon" aria-hidden="true" />
+      <span className="visually-hidden">Previous</span>
+    </button>
+    <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+      <span className="carousel-control-next-icon" aria-hidden="true" />
+      <span className="visually-hidden">Next</span>
+    </button>
   </div>
 </div>
 
-      </div>
+<div/>
+<div/>
         <div className="row my-2">
 <div className="col-md-4 text-center bg-link p-5">
 <div className="card" style={{width: '18rem'}}>
-  <img src="https://images.pexels.com/photos/326055/pexels-photo-326055.jpeg?auto=compress&cs=tinysrgb&w=600" className="card-img-top" width="100%"/>
+  <img src="https://images.pexels.com/photos/326055/pexels-photo-326055.jpeg?auto=compress&cs=tinysrgb&w=600" className="card-img-top" width="100px"/>
   <div className="card-body">
     <h5 className="card-title">Card title</h5>
     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
   </div>
 </div>
 </div>
 <div className="col-md-4 text-center bg-link p-5 ">
 <div className="card" style={{width: '18rem'}}>
-  <img src="https://images.pexels.com/photos/326055/pexels-photo-326055.jpeg?auto=compress&cs=tinysrgb&w=600" className="card-img-top" width="100%" />
+  <img src="https://images.pexels.com/photos/406014/pexels-photo-406014.jpeg?auto=compress&cs=tinysrgb&w=600" className="card-img-top" width="100px" />
   <div className="card-body">
     <h5 className="card-title">Card title</h5>
     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
   </div>
 </div>
 </div>
 <div className="col-md-4 text-center bg-link p-5 ">
 <div className="card" style={{width: '18rem'}}>
-  <img src="https://images.pexels.com/photos/326055/pexels-photo-326055.jpeg?auto=compress&cs=tinysrgb&w=600" className="card-img-top" width="100%" />
+  <img src="https://images.pexels.com/photos/776656/pexels-photo-776656.jpeg?auto=compress&cs=tinysrgb&w=600" className="card-img-top" width="100px" />
   <div className="card-body">
     <h5 className="card-title">Card title</h5>
     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
   </div>
 </div>
 </div>
